93714d41ecacc03e69e0e6a505f66b48bccca7c5
Sam Mister <samuel.mister@bristol.ac.uk>
2024-01-11 17:52:47 +0000

    Thermodynamic Calculation Script Created

---
+++ ThermodynamicCalculations.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 11 17:31:40 2024
+
+@author: sm15883
+"""
+
+'''
+Given a set of system parameters this script uses functions from the TimeEvolver 
+Module to generate the time evolved state rho(t), this combined with the Hamiltonians
+and the dissipation operators gives us the required information to calculate the 
+thermodynamic properties 
+'''
+
+from TimeEvolver import TimeEvolver
+from TimeEvolver import SystemOperators
+
+Np = 5 #Number of Periods 
+TimeStep = 0.1
+
+Time, Results = TimeEvolver(Np,TimeStep)
+Hz,Hx,LZ,LX = SystemOperators()
--- TimeEvolver.py
+++ TimeEvolver.py
@@ -12,8 +12,8 @@ from SystemToolbox import HamiltonianGenerator
 from SystemToolbox import GeneralGlobalME 
 import matplotlib.pyplot as plt
 #Parameters for the closed system
-NumberOfSpins = 4
-Jmean = 1
+NumberOfSpins = 2
+Jmean = np.pi/2
 Jvar = 0
 hmean = np.pi
 hvar = 0.5
@@ -21,7 +21,7 @@ ExternalFieldStength = 0
 
 #Bath parameters 
 s = 1 #This corresponds to an ohmic bath 
-G = 0.1 # This parameter represents the system bath coupling strength
+G = 1 # This parameter represents the system bath coupling strength
 B = 1 # This is the inverse temperature of the bath 
 
 Sx, Sz, Sy = PauliN(NumberOfSpins)
@@ -44,6 +44,9 @@ At this point we have the components require to build the master equations
 needed to evolve the system. Thus I construct a function that takes these objects,
 with a few additional things, and returns the time evolution of the state.
 '''
+def SystemOperators():
+    return Hz,Hx,LZ,LX
+
 def TimeEvolver(Np,TimeStep):
     Tz = 1 #time for z evolution
     Tx = 1 #time for x evolution
@@ -51,23 +54,22 @@ def TimeEvolver(Np,TimeStep):
     TotalTime = np.arange(0,Np*(Tz+Tx),TimeStep)
     TzTime = np.arange(0,Tz,TimeStep)
     TxTime = np.arange(0,Tx,TimeStep)
-    options = qu.Options(nsteps = 1000)
-    psi0Therm = (-B*Hz).expm()/((-B*Hz).expm()).tr()
+    #psi0Therm = (-B*Hz).expm()/((-B*Hz).expm()).tr()
     psi0 = qu.tensor([qu.Qobj([[1/np.sqrt(2)],[1/np.sqrt(2)]]) for n in range(NumberOfSpins)])
     States = []
     for i in range(Np):
         output = qu.mesolve(Hz, psi0, TzTime,LZ,[])# Evolve for the Hz period 
         States += output.states 
-        output1 = qu.mesolve(Hx, output.states[-1], TxTime,LX,[],options = options)# Evolve for the Hx period 
+        output1 = qu.mesolve(Hx, output.states[-1], TxTime,LX,[])# Evolve for the Hx period 
         States += output1.states 
         psi0 = output1.states[-1]
     return TotalTime, States
-
-Time, Results = TimeEvolver(5,0.001)
-
-SxSignature = []
-for i in range(len(Results)):
-    SxSignature.append((Results[i]*Sx[0]).tr().real)
+if __name__ == "__main__":
+    Time, Results = TimeEvolver(5,0.1)
     
-plt.plot(Time,SxSignature)
-plt.show()
+    SxSignature = []
+    for i in range(len(Results)):
+        SxSignature.append((Results[i]*Sx[0]).tr().real)
+        
+    plt.plot(Time,SxSignature)
+    plt.show()
