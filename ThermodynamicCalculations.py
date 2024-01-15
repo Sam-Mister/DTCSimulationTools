deb24d5bb5c23dbd96cfadaf52e70d2eda68e9a2
Sam Mister <samuel.mister@bristol.ac.uk>
2024-01-15 12:41:13 +0000

    Integrated functions for parameter sweeps now added

--- ThermodynamicCalculations.py
+++ ThermodynamicCalculations.py
@@ -17,13 +17,14 @@ The required parameters from the highest level of simulation will be provided in
 dictionary and passed along. Closed system parameters listed first, then bath parameters
 For example considering the following
 '''
-params = {"NumSpins":5, "Jmean":1, "Jvar":0.5, "hmean":3, "ExternalField":0, "s":1,"G":0.1,"B":1}
+params = {"NumSpins":5, "Jmean":1, "Jvar":0.5, "hmean":3,"hvar":0, "ExternalField":0,"Tz":1,"Tx":1, "s":1,"G":0.1,"B":1}
 
 from TimeEvolver import TimeEvolver
 from TimeEvolver import SystemOperators
 from scipy import linalg
 import qutip as qu
 import matplotlib.pyplot as plt
+import numpy as np
 
 '''
 Now we have an array of time steps, an array of density operators at each time step,
@@ -115,8 +116,7 @@ def IntegrationRoutine(Time,Arr,a0):
     return ApproxInt
 
 def PlottingWrapper(xArr,yArr,xlbl,ylbl):
-    T = 2
-    plt.plot(xArr/T,yArr)
+    plt.plot(xArr,yArr)
     plt.xlabel(xlbl)
     plt.ylabel(ylbl)
     plt.show()
@@ -128,18 +128,88 @@ def AbslistDifference(Arr1,Arr2):
         NewList.append(abs(Arr1[i] - Arr2[i]))
     return NewList
 
+def IntergratedProperties(Tag,start,stop,inc,params):
+    #Tag is the string corresponding to the parameter that needs to be swept
+    #I calculate the change over a single period for the first,last and middle period
+    
+    FirstPeriodHeat = []
+    MiddlePeriodHeat = []
+    EndPeriodHeat = []
+    
+    FirstPeriodWork = []
+    MiddlePeriodWork = []
+    EndPeriodWork = []
+    
+    FirstPeriodEnergy = []
+    MiddlePeriodEnergy = []
+    EndPeriodEnergy = []
+    
+    FirstPeriodEntropy = []
+    MiddlePeriodEntropy = []
+    EndPeriodEntropy = []
+    SweepArr = np.arange(start,stop,inc)
+    N = len(SweepArr)
+    for i in range(N):
+        print('Loop {}/{}'.format(i+1,N))
+        params[Tag] = SweepArr[i]
+        
+        Np = 8 #Number of Periods 
+        MidIndex = int(Np/2)
+        FinalIndex = Np-1
+        TimeStep = 0.01
+        Time, Rho = TimeEvolver(Np,TimeStep,params)
+        PeriodEnd = int(len(Time)/Np - 1) 
+        StepAmount = int(len(Time)/Np) 
+        Hz,Hx,LZ,LX,Sx,Sz,Sy = SystemOperators(params)
+        TimeCrystalSignature,DotQ, DotW, DotU, DotS = ThrmodynamicRates(Time,Rho,Hz,Hx,LZ,LX,Sx)
+    
+        DeltaQ = IntegrationRoutine(Time,DotQ,0)
+        FirstPeriodHeat.append(DeltaQ[PeriodEnd] - DeltaQ[0])
+        MiddlePeriodHeat.append(DeltaQ[MidIndex*StepAmount+PeriodEnd] - DeltaQ[MidIndex*StepAmount])
+        EndPeriodHeat.append(DeltaQ[FinalIndex*StepAmount+PeriodEnd] - DeltaQ[FinalIndex*StepAmount])
+        
+        DeltaW = IntegrationRoutine(Time,DotW,0)
+        FirstPeriodWork.append(DeltaW[PeriodEnd] - DeltaW[0])
+        MiddlePeriodWork.append(DeltaW[MidIndex*StepAmount+PeriodEnd] - DeltaW[MidIndex*StepAmount])
+        EndPeriodWork.append(DeltaW[FinalIndex*StepAmount+PeriodEnd] - DeltaW[FinalIndex*StepAmount])
+        
+        DeltaU= IntegrationRoutine(Time,DotU,0)
+        FirstPeriodEnergy.append(DeltaU[PeriodEnd] - DeltaU[0])
+        MiddlePeriodEnergy.append(DeltaU[MidIndex*StepAmount+PeriodEnd] - DeltaU[MidIndex*StepAmount])
+        EndPeriodEnergy.append(DeltaU[FinalIndex*StepAmount+PeriodEnd] - DeltaU[FinalIndex*StepAmount])
+        
+        DeltaS = IntegrationRoutine(Time,DotS,0)  
+        FirstPeriodEntropy.append(DeltaS[PeriodEnd] - DeltaS[0])
+        MiddlePeriodEntropy.append(DeltaS[MidIndex*StepAmount+PeriodEnd] - DeltaS[MidIndex*StepAmount])
+        EndPeriodEntropy.append(DeltaS[FinalIndex*StepAmount+PeriodEnd] - DeltaS[FinalIndex*StepAmount])
+        
+        HeatChangeArr = [FirstPeriodHeat,MiddlePeriodHeat,EndPeriodHeat]
+        WorkChangeArr = [FirstPeriodWork,MiddlePeriodWork,EndPeriodWork]
+        EnergyChangeArr = [FirstPeriodEnergy,MiddlePeriodEnergy,EndPeriodEnergy]
+        EntropyChangeArr = [FirstPeriodEntropy,MiddlePeriodEntropy,EndPeriodEntropy]
+        
+    return SweepArr, HeatChangeArr, WorkChangeArr, EnergyChangeArr, EntropyChangeArr
+
 if __name__ == "__main__":
-    Np = 6 #Number of Periods 
+    '''
+    The required parameters from the highest level of simulation will be provided in a 
+    dictionary and passed along. Closed system parameters listed first, then bath parameters
+    For example considering the following
+    '''
+    params = {"NumSpins":5, "Jmean":1, "Jvar":0.5, "hmean":3,"hvar":0, "ExternalField":0,"Tz":1,"Tx":1, "s":1,"G":0.1,"B":1}
+    
+    Np = 8 #Number of Periods 
     TimeStep = 0.01
-    Time, Rho = TimeEvolver(Np,TimeStep)
-    Hz,Hx,LZ,LX,Sx, Sz, Sy = SystemOperators()
+    T = params["Tz"] + params["Tx"]
+    Time, Rho = TimeEvolver(Np,TimeStep,params)
+    Hz,Hx,LZ,LX,Sx,Sz,Sy = SystemOperators(params)
     TimeCrystalSignature,DotQ, DotW, DotU, DotS = ThrmodynamicRates(Time,Rho,Hz,Hx,LZ,LX,Sx)
     
-    PlottingWrapper(Time,TimeCrystalSignature,"t/T",r"$\langle \sigma_{1}^{x}(t)\rangle$")
-    PlottingWrapper(Time,DotQ,"t/T",r"$\dot{Q}(t)$")
-    PlottingWrapper(Time,DotW,"t/T",r"$\dot{W}(t)$")
-    PlottingWrapper(Time,DotU,"t/T",r"$\dot{U}(t)$")
-    PlottingWrapper(Time,DotS,"t/T",r"$\dot{S}(t)$")
+    PlottingWrapper(Time/T,TimeCrystalSignature,"t/T",r"$\langle \sigma_{1}^{x}(t)\rangle$")
+    PlottingWrapper(Time/T,DotQ,"t/T",r"$\dot{Q}(t)$")
+    PlottingWrapper(Time/T,DotW,"t/T",r"$\dot{W}(t)$")
+    PlottingWrapper(Time/T,DotU,"t/T",r"$\dot{U}(t)$")
+    PlottingWrapper(Time/T,DotS,"t/T",r"$\dot{S}(t)$")
     
     '''
     To verify that the previous integration routine makes sense to some degree
@@ -160,13 +230,13 @@ if __name__ == "__main__":
     DeltaU= IntegrationRoutine(Time,DotU,0)
     DeltaS = IntegrationRoutine(Time,DotS,0)
     
-    PlottingWrapper(Time,DeltaQ,"t/T",r"$\Delta Q(t)$")
-    PlottingWrapper(Time,DeltaW,"t/T",r"$\Delta W(t)$")
-    PlottingWrapper(Time,DeltaU,"t/T",r"$\Delta U(t)$")
-    PlottingWrapper(Time,DeltaS,"t/T",r"$\Delta S(t)$")
+    PlottingWrapper(Time/T,DeltaQ,"t/T",r"$\Delta Q(t)$")
+    PlottingWrapper(Time/T,DeltaW,"t/T",r"$\Delta W(t)$")
+    PlottingWrapper(Time/T,DeltaU,"t/T",r"$\Delta U(t)$")
+    PlottingWrapper(Time/T,DeltaS,"t/T",r"$\Delta S(t)$")
     
-    PlottingWrapper(Time,AbslistDifference(DeltaS,EntropyTestArr),"t/T","Error in S")
-    PlottingWrapper(Time,AbslistDifference(DeltaU,EnergyTestArr),"t/T","Error in U")
+    PlottingWrapper(Time/T,AbslistDifference(DeltaS,EntropyTestArr),"t/T","Error in S")
+    PlottingWrapper(Time/T,AbslistDifference(DeltaU,EnergyTestArr),"t/T","Error in U")
     
     '''
     Not I want to be able to look at the integrated quantities over different periods of the evolution
@@ -174,4 +244,19 @@ if __name__ == "__main__":
     then return the intergrated values
     '''
     
+    SweepArr, HeatChangeArr, WorkChangeArr, EnergyChangeArr, EntropyChangeArr = IntergratedProperties("G",0.000001,2.01,0.01,params)
     
+    PlottingWrapper(SweepArr,HeatChangeArr[0],r"$\Gamma$",r"$\Delta Q$")
+    PlottingWrapper(SweepArr,WorkChangeArr[0],r"$\Gamma$",r"$\Delta W$")
+    PlottingWrapper(SweepArr,EnergyChangeArr[0],r"$\Gamma$",r"$\Delta U$")
+    PlottingWrapper(SweepArr,EntropyChangeArr[0],r"$\Gamma$",r"$\Delta S$")
+    
+    PlottingWrapper(SweepArr,HeatChangeArr[1],r"$\Gamma$",r"$\Delta Q$")
+    PlottingWrapper(SweepArr,WorkChangeArr[1],r"$\Gamma$",r"$\Delta W$")
+    PlottingWrapper(SweepArr,EnergyChangeArr[1],r"$\Gamma$",r"$\Delta U$")
+    PlottingWrapper(SweepArr,EntropyChangeArr[1],r"$\Gamma$",r"$\Delta S$")
+    
+    PlottingWrapper(SweepArr,HeatChangeArr[2],r"$\Gamma$",r"$\Delta Q$")
+    PlottingWrapper(SweepArr,WorkChangeArr[2],r"$\Gamma$",r"$\Delta W$")
+    PlottingWrapper(SweepArr,EnergyChangeArr[2],r"$\Gamma$",r"$\Delta U$")
+    PlottingWrapper(SweepArr,EntropyChangeArr[2],r"$\Gamma$",r"$\Delta S$")
--- TimeEvolver.py
+++ TimeEvolver.py
@@ -11,51 +11,37 @@ from SystemToolbox import disorderGen
 from SystemToolbox import HamiltonianGenerator
 from SystemToolbox import GeneralGlobalME 
 import matplotlib.pyplot as plt
-#Parameters for the closed system
-NumberOfSpins = 2
-Jmean = np.pi/2
-Jvar = 0.5
-hmean = np.pi
-hvar = 0.01
-ExternalFieldStength = 0
-
-#Bath parameters 
-s = 1 #This corresponds to an ohmic bath 
-G = 0.1 # This parameter represents the system bath coupling strength
-B = 1 # This is the inverse temperature of the bath 
-
-Sx, Sz, Sy = PauliN(NumberOfSpins)
-
-Jarr, harr = disorderGen(Jmean,Jvar,hmean,hvar,NumberOfSpins)
-
-Hz,Hx = HamiltonianGenerator(Jarr, harr, ExternalFieldStength, Sx, Sz)
-
-'''
-We assume that each spin of the model couples to the bath equally 
-'''
-VAll = qu.Qobj() #defines the operator through with the system couples to the enviroment. 
-for i in range(len(Sz)):
-    VAll += Sz[i]
-
-LX,LZ = GeneralGlobalME(Hx,Hz,s,G,B,VAll)
 
 '''
 At this point we have the components require to build the master equations
 needed to evolve the system. Thus I construct a function that takes these objects,
 with a few additional things, and returns the time evolution of the state.
 '''
-def SystemOperators():
+def SystemOperators(params):
+    #params = {"NumSpins":5, "Jmean":1, "Jvar":0.5, "hmean":3,"hvar":0,ExternalField":0, "s":1,"G":0.1,"B":1}
+
+    Sx, Sz, Sy = PauliN(params["NumSpins"])
+
+    Jarr, harr = disorderGen(params["Jmean"],params["Jvar"],params["hmean"],params["hvar"],params["NumSpins"])
+
+    Hz,Hx = HamiltonianGenerator(Jarr, harr, params["ExternalField"], Sx, Sz)
+    
+    VAll = qu.Qobj() #defines the operator through with the system couples to the enviroment. 
+    for i in range(len(Sz)):
+        VAll += Sz[i]
+    
+    LX,LZ = GeneralGlobalME(Hx,Hz,params["s"],params["G"],params["B"],VAll)
     return Hz,Hx,LZ,LX,Sx, Sz, Sy
 
-def TimeEvolver(Np,TimeStep):
-    Tz = 1 #time for z evolution
-    Tx = 1 #time for x evolution
+def TimeEvolver(Np,TimeStep,params):
+    Hz,Hx,LZ,LX,Sx, Sz, Sy = SystemOperators(params)
+    T = params["Tz"] + params["Tx"]
     
-    TotalTime = np.arange(0,Np*(Tz+Tx),TimeStep)
-    TzTime = np.arange(0,Tz,TimeStep)
-    TxTime = np.arange(0,Tx,TimeStep)
-    #psi0Therm = (-B*Hz).expm()/((-B*Hz).expm()).tr()
-    psi0 = qu.tensor([qu.Qobj([[1/np.sqrt(2)],[1/np.sqrt(2)]]) for n in range(NumberOfSpins)])
+    TotalTime = np.arange(0,Np*(T),TimeStep)
+    TzTime = np.arange(0,params["Tz"],TimeStep)
+    TxTime = np.arange(0,params["Tx"],TimeStep)
+    psi0Therm = (-params["B"]*Hz).expm()/((-params["B"]*Hz).expm()).tr()
+    psi0 = psi0Therm#qu.tensor([qu.Qobj([[1/np.sqrt(2)],[1/np.sqrt(2)]]) for n in range(params["NumSpins"])])
     States = []
     for i in range(Np):
         output = qu.mesolve(Hz, psi0, TzTime,LZ,[])# Evolve for the Hz period 
@@ -65,7 +51,23 @@ def TimeEvolver(Np,TimeStep):
         psi0 = output1.states[-1]
     return TotalTime, States
 if __name__ == "__main__":
-    Time, Results = TimeEvolver(5,0.1)
+    #Parameters for the closed system
+    NumberOfSpins = 2
+    Jmean = np.pi/2
+    Jvar = 0.5
+    hmean = np.pi
+    hvar = 0.01
+    ExternalFieldStength = 0
+
+    #Bath parameters 
+    s = 1 #This corresponds to an ohmic bath 
+    G = 0.1 # This parameter represents the system bath coupling strength
+    B = 1 # This is the inverse temperature of the bath 
+    params = {"NumSpins":NumberOfSpins, "Jmean":Jmean, "Jvar":Jvar, "hmean":hmean,"hvar":hvar,"ExternalField":ExternalFieldStength,"Tz":1,"Tx":1,"s":s,"G":G,"B":B}
+    
+    Hz,Hx,LZ,LX,Sx, Sz, Sy = SystemOperators(params)
+    
+    Time, Results = TimeEvolver(5,0.01,params)
     
     SxSignature = []
     for i in range(len(Results)):
